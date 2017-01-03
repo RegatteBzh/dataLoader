6d91700ae53ec92d2f5071f417da17efca127954
Cyrille M <cmeichel@free.fr>
2017-01-03 22:12:31 +0100

    load all winds

--- wind/cmd.go
+++ wind/cmd.go
@@ -2,8 +2,11 @@ package wind
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
+	"path"
+	"regexp"
 
 	redis "gopkg.in/redis.v4"
 
@@ -12,6 +15,13 @@ import (
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
+
+type fileLoader struct {
+	Filename string
+	Handler  *os.File
+	WindsU   []float32
+	WindsV   []float32
+}
 
 func init() {
 	flags := MainCmd.Flags()
@@ -24,45 +34,72 @@ func init() {
 
 }
 
+func loadAllWinds(windPath string, redisName string, fake bool) (err error) {
+
+	var client *redis.Client
+
+	if !fake {
+		client = database.Open()
+		defer client.Close()
+	} else {
+		fmt.Printf("Fake mode.\n")
+	}
+
+	progressBars, err := multibar.New()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	files, err := ioutil.ReadDir(windPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	filter, err := regexp.Compile(`^gfs\..*\.f(\d{3})\.bin$`)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	progressBars.Printf("Loading WindForecast %s\n", windPath)
+	progressBar := progressBars.MakeBar(180, "Wind")
+
+	filenames := make(map[string]*fileLoader)
+	for _, f := range files {
+		match := filter.FindStringSubmatch(f.Name())
+		if len(match) > 0 {
+			filename := path.Join(windPath, f.Name())
+			file, err := os.Open(filename)
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer file.Close()
+			windLoader := fileLoader{
+				Handler:  file,
+				Filename: filename,
+			}
+			filenames[match[1]] = &windLoader
+		}
+	}
+
+	if !fake {
+		go progressBars.Listen()
+	}
+
+	err = LoadAll(filenames, client, redisName, progressBar, fake)
+
+	return
+
+}
+
 // MainCmd is the main command manager
 var MainCmd = &cobra.Command{
-	Use:   "wind <path>",
+	Use:   "wind <folder-path>",
 	Short: "Load wind forecast",
 	Run: func(cmd *cobra.Command, args []string) {
-		var client *redis.Client
 
 		fake := viper.GetBool("fake")
 
-		if !fake {
-			client = database.Open()
-			defer client.Close()
-		} else {
-			fmt.Printf("Fake mode.\n")
-		}
-
-		if len(args) == 0 {
-			log.Fatal("Missing file")
-		}
-
-		progressBars, err := multibar.New()
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		progressBars.Printf("Loading WindForecast %s\n", args[0])
-		progressBar := progressBars.MakeBar(180, "Wind")
-
-		file, err := os.Open(args[0])
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer file.Close()
-
-		if !fake {
-			go progressBars.Listen()
-		}
-
-		err = Loader(file, client, viper.GetString("wind_name"), progressBar, fake)
+		loadAllWinds(args[0], viper.GetString("wind_name"), fake)
 
 	},
 }
--- wind/loader.go
+++ wind/loader.go
@@ -7,6 +7,9 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"strconv"
+
+	"errors"
 
 	"github.com/sethgrid/multibar"
 	redis "gopkg.in/redis.v4"
@@ -67,11 +70,6 @@ func Loader(file io.Reader, c *redis.Client, redisName string, progressBar multi
 		count = countV
 	}
 
-	//windMap := mapper.New(image.Rect(0, 0, 360, 181), 60, 60)
-
-	progress := 1
-	lat := 90
-	lon := 0
 	for i := uint32(0); i < count; i++ {
 		speed := Speed{
 			SpeedU: windsU[i],
@@ -83,25 +81,7 @@ func Loader(file io.Reader, c *redis.Client, redisName string, progressBar multi
 			log.Fatal("Cannot convert wind speed to json")
 		}
 
-		var loc redis.GeoLocation
-		if lat >= -85 && lat <= 85 {
-			if lon >= 180 {
-				// lon[180 - 359] => x=[0-179]
-				loc = redis.GeoLocation{
-					Longitude: float64(lon) - 360,
-					Latitude:  float64(lat),
-					Name:      string(jsonSpeed),
-				}
-
-			} else {
-				// lon[0 - 179] => x=[180-359]
-				loc = redis.GeoLocation{
-					Longitude: float64(lon),
-					Latitude:  float64(lat),
-					Name:      string(jsonSpeed),
-				}
-			}
-
+		if loc, err := makePoint(string(jsonSpeed), i); err != nil {
 			if !fake {
 				c.GeoAdd(redisName, &loc)
 			} else {
@@ -109,17 +89,93 @@ func Loader(file io.Reader, c *redis.Client, redisName string, progressBar multi
 			}
 		}
 
-		lon++
-
-		if lon >= 360 {
-			lat--
-			lon = 0
-			progress++
-			if !fake {
-				progressBar(progress)
-			}
+		if i%360 == 0 {
+			progressBar(int(i) / 360)
 		}
 	}
 
 	return err
+}
+
+func makePoint(name string, index uint32) (redis.GeoLocation, error) {
+	lon := index % 360
+	lat := 90 - int(index)/360
+
+	if lat >= -85 && lat <= 85 {
+		if lon >= 180 {
+			// lon[180 - 359] => x=[0-179]
+			return redis.GeoLocation{
+				Longitude: float64(lon) - 360,
+				Latitude:  float64(lat),
+				Name:      name,
+			}, nil
+
+		}
+		// lon[0 - 179] => x=[180-359]
+		return redis.GeoLocation{
+			Longitude: float64(lon),
+			Latitude:  float64(lat),
+			Name:      name,
+		}, nil
+	}
+	return redis.GeoLocation{}, errors.New("out of bounds")
+}
+
+// LoadAll loads all weather forecast
+func LoadAll(files map[string]*fileLoader, client *redis.Client, redisName string, progressBar multibar.ProgressFunc, fake bool) (err error) {
+	var count uint32
+
+	if !fake {
+		client.Del(redisName)
+		progressBar(0)
+	}
+
+	for _, descriptor := range files {
+		countU, windsU, errU := readBlock(descriptor.Handler)
+		if errU != nil {
+			log.Fatal(errU)
+		}
+		_, windsV, errV := readBlock(descriptor.Handler)
+		if errV != nil {
+			log.Fatal(errV)
+		}
+		count = countU
+		descriptor.WindsU = windsU
+		descriptor.WindsV = windsV
+	}
+
+	for i := uint32(0); i < count; i++ {
+
+		speeds := make(map[int]Speed)
+
+		for index, descriptor := range files {
+			indexInt, err := strconv.Atoi(index)
+			if err != nil {
+				log.Fatal(err)
+			}
+			speeds[indexInt] = Speed{
+				SpeedU: descriptor.WindsU[i],
+				SpeedV: descriptor.WindsV[i],
+			}
+		}
+
+		jsonSpeed, err := json.Marshal(speeds)
+		if err != nil {
+			log.Fatal("Cannot convert wind speed to json")
+		}
+
+		if loc, err := makePoint(string(jsonSpeed), i); err == nil {
+			if !fake {
+				client.GeoAdd(redisName, &loc)
+			} else {
+				fmt.Printf("[%.03f, %.03f] %s\n", loc.Longitude, loc.Latitude, loc.Name)
+			}
+		}
+
+		if i%360 == 0 && !fake {
+			progressBar(int(i) / 360)
+		}
+	}
+
+	return
 }
